af59d3f865e6fed398ba742c5a6ae901031a18c1
whateverpal <an@nlomarketing.ru>
2012-04-06 18:41:39 +0400

    main-screen.js is document-ready again

--- main_spy/static/js/ui/main-screen.js
+++ main_spy/static/js/ui/main-screen.js
@@ -2,6 +2,8 @@ new Module('ui/main-screen.js', ['jsage/eventbus.js', 'ui/operations-counter.js'
 
 console.log("Main screen - group spy")
 
+$(document).ready(function () {
+	
 // operations counter
 var opCounter1 = groupspy.OperationsCounter.create($("#operations-counter")[0])
 
@@ -35,48 +37,7 @@ $(window).resize(function () {
 	adjust_elements()
 })	
 adjust_elements()
-return
 
-var screens_info = {
-	'screen-stat': {
-		address: 'group'
-	},
-	'screen-post': {
-		address: 'posts'
-	},
-	'screen-group-comparison': {
-		address: 'group-comparison'
-	},
-	'screen-usertop': {
-		address: 'usertop'
-	},
-	'screen-sa_distribution': {
-		address: 'sa_distribution'
-	}		
-}
-	
-function redrawFrame() {
-	if (group_shown != null || current_screen == "screen-group-comparison") {
-		if (current_screen_shown != null)
-			current_screen_shown.style.display = 'none'
-
-		current_screen_shown = document.getElementById('central-' + screens_info[current_screen].address)
-		current_screen_shown.style.display = 'block'
-		jsage.global_bus.trigger(groupspy.messages[screens_info[current_screen].address + '_frame_activate'], group_shown)
-		
-		$("div.group_asking_to_click")[0].style.display = "none"
-		if (curSelected != null) 
-			$(curSelected)[0].style.color = ""
-		if (group_shown != null) {
-			$("#group_alias_" + group_shown)[0].style.color = "#ff6600"
-			curSelected = $("#group_alias_" + group_shown)[0]	
-		}
-		
-		for (var e in screens_info) {
-			var header = $('#' + e + '-header')[0]
-			header.style.display = e == current_screen ? 'block' : 'none'
-		}
-	}
-}
+})
 
 })
